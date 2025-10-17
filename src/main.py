b19173e97aff5dea4f123b39cb2f3d344b3e877f
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-17 19:01:09 +0200

    3. TextNode

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    test = TextNode("Anchor text", TextType.LINK, "https://test.com")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
