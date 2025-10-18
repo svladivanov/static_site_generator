6bb3ead71e4d19242bac2dfc8ab5b983adfc51ba
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-18 13:23:08 +0200

    8. Split Delimiter

---
+++ src/inline_markdown.py
@@ -0,0 +1,24 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_text = old_node.text.split(delimiter)
+        if len(split_text) % 2 == 0:
+            raise Exception("improper markdown, formatted section not closed")
+        result = []
+        for i in range(len(split_text)):
+            if split_text[i] == "":
+                continue
+            if i % 2 == 0:
+                result.append(TextNode(split_text[i], TextType.TEXT))
+            else:
+                result.append(TextNode(split_text[i], text_type))
+        new_nodes.extend(result)
+    return new_nodes
--- src/textnode.py
+++ src/textnode.py
@@ -13,7 +13,7 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type: TextType, url=None) -> None:
+    def __init__(self, text: str, text_type: TextType, url=None) -> None:
         self.text = text
         self.text_type = text_type
         self.url = url
